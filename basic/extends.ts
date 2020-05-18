1431bc005ddc56ae9d68d92188f82dc4d62ad204
Jack <1105521265@qq.com>
2020-05-18 19:54:47 +0800

    add extends.js

---
+++ basic/extends.ts
@@ -0,0 +1,36 @@
+/**
+ * 继承
+ */
+class Father {
+  static staticFatherName = 'FatherName';
+  static staticGetFatherName = function () {
+    console.log(Father.staticFatherName);
+  }
+  constructor(public name) {
+    this.name = name;
+  }
+  getName() {
+    console.log(this.name);
+  }
+}
+
+class Child extends Father {
+  static staticChildName = 'ChildName';
+  static staticGetChildName = function () {
+    console.log(Child.staticChildName);
+  }
+  constructor(public name, public age) {
+    super(name);
+    this.age = age;
+  }
+  getAge() {
+    console.log(this.age);
+  }
+}
+
+let child = new Child('zhangsan', 20);
+child.getName();
+child.getAge();
+Child.staticGetChildName();
+Child.staticGetFatherName();
+export { };
