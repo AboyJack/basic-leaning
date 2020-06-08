95616145db16b681114246f763007bcd509ae1e9
Jack <1105521265@qq.com>
2020-06-08 09:26:55 +0800

    add typescript

---
+++ typescript/1.ts/4.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * 装饰器
+ */
+var a;
+(function (a) {
+    /**
+     * 类装饰器
+     */
+    var Person = /** @class */ (function () {
+        function Person() {
+        }
+        return Person;
+    }());
+})(a || (a = {}));
---
+++ typescript/1.ts/4.ts
@@ -0,0 +1,13 @@
+/**
+ * 装饰器
+ */
+namespace a {
+  /**
+   * 类装饰器
+   */
+  class Person {
+    constructor() {
+
+    }
+  }
+}
